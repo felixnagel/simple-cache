3a14fc525b81fb17edbe26689008c68cd19aa89d
fnagel <felixnagel@users.noreply.github.com>
2016-06-20 18:39:08 +0200

    Add files via upload

--- src/Text.php
+++ src/Text.php
@@ -7,30 +7,29 @@ class Text{
 	private $_sFolderPath;
 	private $_iExpiresInDays;
 
-	public function __construct($sFolderPath, $iExpiresInDays = 30){
-		$this->_sFolderPath = Helper\Path::to_path_part($sFolderPath);
+	public function __construct($sFolderPath, $iExpiresInHours = 0){
+		$this->_sFolderPath = $sFolderPath;
 		$this->_iExpiresInDays = $iExpiresInDays;
 		if(!is_dir($sFolderPath)){
-			mkdir($sFolderPath, 0644, true);
+			mkdir($sFolderPath, 0755, true);
 		}
 	}
 
-	public function get($sFileId){
-		$sFilePath = $this->_sFolderPath.DIRECTORY_SEPARATOR.$sFileId;
+	public function get_cached_path($sRequestId){
+		$sFilePath = $this->_sFolderPath.DIRECTORY_SEPARATOR.$sRequestId;
 		if(file_exists($sFilePath)){
-	     	$iAgeDays = (time() - filemtime($sFilePath)) / (60*60*24);
+	     	$iAgeDays = (time() - filemtime($sFilePath)) / (60*60);
 	     	if($iAgeDays >= $this->_iExpiresInDays){
 	     		unlink($sFilePath);
 	     	}else{
-	     		return file_get_contents($sFilePath);
+	     		return $sFilePath;
 	     	}
 	    }
-
     	return false;
 	}
 
-	public function write($sFileId, $sContent){
-		$sFilePath = $this->_sFolderPath.DIRECTORY_SEPARATOR.$sFileId;
+	public function write($sRequestId, $sContent){
+		$sFilePath = $this->_sFolderPath.DIRECTORY_SEPARATOR.$sRequestId;
 		file_put_contents($sFilePath, $sContent);
 		return $sFilePath;
 	}
